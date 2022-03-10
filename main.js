4046b2be7649db65e480fd7ad9cf4888e6e714f5
idontknowwhyiexist <96338346+idontknowwhyiexist@users.noreply.github.com>
2022-03-10 15:57:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var firstname="Arav";
+console.log(firstname);
+var lastname=" Nath";
+console.log(lastname);
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+
+var replacename=firstname.replace("Arav","New Name");
+console.log(replacename);
+console.log("I am in console screen");
